704e6717fa09be4280b1aca6f595bc4bf0907153
ianchan3 <ianchan3138@gmail.com>
2023-02-20 21:47:39 -0800

    First Commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll)
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+  closeMenu();
+}
+
+const menuWrapper = document.querySelector(".nav-wrapper");
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+const showMenu = () => {
+  hamburger.style.display = "none";
+  close.style.transform = "translateY(0)";
+  menuWrapper.style.transform = "translateX(0)";
+  menu.style.transform = "translateX(0)";
+}
+
+const closeMenu = () => {
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menuWrapper.style.transform = "translateX(-200%)";
+  menu.style.transform = "translateX(200%)";
+
+}
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", closeMenu);
+menuWrapper.addEventListener("click", closeMenu)
